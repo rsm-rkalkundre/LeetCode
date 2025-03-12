e044003727f485333d43b6ec384ed3921f2c65a8
Rohit Udayraj Kalkundre <141078164+rsm-rkalkundre@users.noreply.github.com>
2025-03-12 15:59:54 -0700

    Time: 1082 ms (47.11%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,9 +1,11 @@
--- Write your PostgreSQL query statement below
-SELECT employee_id
+# Write your MySQL query statement below
+(SELECT employee_id
 FROM Employees
-WHERE employee_id NOT IN (select employee_id FROM Salaries)
+WHERE employee_id NOT IN (SELECT employee_id FROM Salaries))
 UNION
+(
 SELECT employee_id
 FROM Salaries
-WHERE employee_id NOT IN (select employee_id FROM Employees)
-order by 1
+WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+)
+ORDER BY 1
