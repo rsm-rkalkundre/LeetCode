a33a6d9e6b51780f14959d0ef7648bd2ffb391b4
Rohit Udayraj Kalkundre <141078164+rsm-rkalkundre@users.noreply.github.com>
2025-03-14 20:58:43 -0700

    Time: 2277 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with cte as
+(select *, id - row_number() over () as id_diff
+from stadium
+where people > 99)
+select id, visit_date, people
+from cte
+where id_diff IN (select id_diff from cte group by id_diff having count(*) > 2)
